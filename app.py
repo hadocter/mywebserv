a5ff4ba5383f897ff1a1d934093684d82cb5599f
HACW <hadoter@gmail.com>
2020-02-12 14:29:10 +0000

    LOLOLOL sexyharam

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template
 import os
 app = Flask(__name__)
+if __name__ == '__main__':
+    app.run(
+            host="nasfamilytest1.herokuapp.com",port=5000)
 
 @app.route('/')
 def hello():
